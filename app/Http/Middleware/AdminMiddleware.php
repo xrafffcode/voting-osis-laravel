80b3285dcf92ff02a594dcf2986eefa6e6c7b0d5
Muhamad Rafli <yusfiwawan2@gmail.com>
2022-07-19 21:30:34 +0700

    ✨  Add Middleware Admin

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,10 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if ($request->user()->role == User::ROLE_ADMIN) {
+            return $next($request);
+        }
+        
+        abort(401, 'You are not authorized to access this page.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,8 @@ use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+
